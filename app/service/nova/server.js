cb5efdb7e880269838ac4c98a620beaf953b28be
gogocorw <gogocrow@163.com>
2017-05-19 15:30:07 +0800

    support instance resize.

--- app/service/nova/server.js
+++ app/service/nova/server.js
@@ -21,15 +21,39 @@ module.exports = app => {
      * @param {*String} region 
      */
     async getProductName(service, tag, body, catalogs, region) {
-      const flavorRef = body.server.flavorRef;
-      return `nova:server:${flavorRef}`;
+      if (body.server) {
+        const flavorRef = body.server.flavorRef;
+        return `nova:server:${flavorRef}`;
+      } else if (body.resize) {
+        const flavorRef = body.resize.flavorRef;
+        return `nova:server:${flavorRef}`;
+      }
     }
+
     getProjectId(resource) {
       return resource['tenant_id'];
     }
 
     formAPIQueryStr(service, tag, obj, rest) {
       return `${obj.endpoint}/${tag}s/detail?all_tenants=1&flavor=${rest[0]}`;
+    }
+    
+    /**
+     * Parse the uuid out from an option.
+     * @param {*Options} opt 
+     */
+    parsePutUUID(opt) {
+      if (/\/action/.test(opt.requestUrl)) {
+        return opt.requestUrl.replace(/\/$/, '').replace(/^(.*)\/servers\//, '').replace(/\/action(.*)$/, '');
+      }
+      return super.parsePutUUID(opt);
+    }
+
+    async POST(opt) {
+      if (opt.request.resize) {
+        return await this.PUT(opt);
+      }
+      return await super.POST(opt);
     }
   }
   return Server;
