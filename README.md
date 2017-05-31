1f3c060eb6bba186b87c6fd93fc2164c8e3d0c5e
gogocorw <gogocrow@163.com>
2017-05-31 09:10:08 +0800

    configuration updated

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ This is a charge service for the OpenStack. It can do the followings:
 
 # Deployment
 
-There are two parts to deployment. 
+There are 3 parts to deployment. 
 1. The WSGI middleware for the OpenStack. It should be installed in every API node in the OpenStack.
 2. The charge system implementation. It can be installed everywhere. But better under a Ngix or HA Proxy.
+3. Before you start, make sure you put your configuration under folder config. The config/config.default.js is the basic configuration for the app to start. You need to put the other configurations like db/memcached in your env configuration. Please follow this step:
+  1. Name your env by the command: 
+```
+export EGG_SERVER_ENV="local"
+```
+  2. Create your configuration file according to the env. Here the name should be config/config.local.js
 
 ## Enviroment
 
@@ -35,4 +41,3 @@ pip install .
 node script/setup.js
 ```
 Note: You must use the properly user to run this command.
-4. 
---
+++ config/config.default.js
@@ -0,0 +1,46 @@
+'use strict';
+
+exports.keys = 'some secret hurr';
+exports.chargeModule = {
+  "modules": [{
+    "name": "keystone",
+    "keyField": "pipeline",
+    "api": "keystone-paste.ini",
+    "service": "httpd",
+    "ignore": ["pipeline:public_version_api", "pipeline:admin_version_api"]
+  }, {
+    "name": "nova"
+  }, {
+    "name": "cinder"
+  }, {
+    "name": "neutron",
+    "service": "neutron-server"
+  }, {
+    "name": "glance",
+    "service": "openstack-glance-api",
+    "api": "glance-api-paste.ini",
+    "keyField": "pipeline",
+    "ignore": ["pipeline:glance-api", "pipeline:glance-api-caching", "pipeline:glance-api-cachemanagement"]
+  }],
+  "key": "billing",
+  "filter": {
+    "paste.filter_factory": "billingmiddleware.billing:filter_factory",
+    "billing_wsgi_url": "http://lb.2.stage.polex.io/${module}",
+    "pre_ignore_method": "HEAD, TRACE, GET",
+    "post_ignore_method": "HEAD, TRACE, GET"
+  },
+  "keycontext": ["keystonecontext", "authtoken", "context", "json_body", "url_normalize"]
+};
+/** 是否需要扫描旧表格，并且同步数据。默认为false，不需要。 */
+exports.requireMerge = true;
+
+exports.middleware = ['auth'];
+
+const modules = exports.chargeModule.modules;
+exports.auth = {
+  ignoreLink: modules.map(module => module.name),
+};
+
+exports.errorHandler = {
+  file: '/var/log/charge/error.log',
+};
