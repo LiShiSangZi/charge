ef3147816385938790336e0fe0937bb45d6552fe
longpeng <longpeng@polex.com.cn>
2017-07-18 14:24:51 +0800

    fix error handler about status.

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -11,7 +11,7 @@ module.exports = (options, app) => {
       let {status, message, requestUrl, responseError} = getErrResObj(e.response);
 
       if (!status) {
-        status = 500;
+        status = e.status || 500;
       }
       if (!message && e.message) {
         message = e.message;
